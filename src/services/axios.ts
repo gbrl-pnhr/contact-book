3e40cc32f39b3b364d636d24f059c506b2f633e7
Gabriel <mxbiel2000@gmail.com>
2024-07-02 16:35:40 -0300

    implementation of Axios interceptors

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,38 @@
-import axios, {type AxiosInstance} from "axios";
+import axios, {type AxiosInstance, type AxiosRequestConfig} from "axios";
 
-const api = axios.create({
-    baseURL: "https://6674787a75872d0e0a968ff7.mockapi.io/api/v1",
-});
+function apiConfig(baseUrl: string): AxiosRequestConfig {
+    return {
+      baseURL: baseUrl,
+    };
+  }
+
+function initAxios(config: AxiosRequestConfig, token?:any){
+    const instance = axios.create(config);
+    instance.interceptors.request.use(
+        (request) =>{
+            return request;
+        },
+        function (error) {
+            return Promise.reject(error);
+        }
+    )
+
+    instance.interceptors.response.use(
+        (response) => {
+            return response;
+        },
+        function (error) {
+            alert(error);
+            return Promise.reject(error);
+        }
+    );
+
+    return instance;
+}
+
+function api(baseURL= "/api", token?: any) {
+    return initAxios(apiConfig(baseURL), token);
+  }
+
 
 export default api;
