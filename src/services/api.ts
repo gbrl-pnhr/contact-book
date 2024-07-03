c2977c05df5e28ad1defda40d46c83d2e7d71477
Gabriel <mxbiel2000@gmail.com>
2024-07-03 15:26:58 -0300

    implementation of get and post functions using observables

--- src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios, {type AxiosInstance} from "axios";
-
-const instance = axios.create({
-    baseURL: '/api/contacBook', // "https://6674787a75872d0e0a968ff7.mockapi.io/api/v1",
-});
-
-export default instance;
-
--- src/services/contacts/typesContacts.ts
+++ src/services/contacts/typesContacts.ts
@@ -4,10 +4,3 @@ export type contactBook = {
     phoneNumber:string,
     email:string,
 };
-
-export type InputCreateContact = {
-    id: number;
-    name: string;
-    phoneNumber:string;
-    email:string;
-  };
