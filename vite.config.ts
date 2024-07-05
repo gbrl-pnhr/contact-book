e864d288a6af6cf8a341218ad6cd9da5e4b796d4
victor <manoelvictortimbo@gmail.com>
2024-07-05 16:16:27 -0300

    feat:configure vite along with .env file

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,28 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://6674787a75872d0e0a968ff7.mockapi.io/api/v1',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
+export default defineConfig(({mode}) => {
+  process.env = {...process.env,...loadEnv(mode, process.cwd())}
+
+  return defineConfig({
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_BASE_URL ,
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, '')
+        },
       },
     },
-  },
+  })
 })
